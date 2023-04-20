6697cd0b18d7f676c0601574be2a7ae64da10003
Kevin Nie <kevinnie2003@163.com>
2023-04-20 16:45:27 -0700

    lab

--- StringServer.java
+++ StringServer.java
@@ -6,21 +6,21 @@ class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
     ArrayList<String> str = new ArrayList<String>();
-
     public String handleRequest(URI url) {
         if (url.getPath().contains("/add-message")) {
             String[] parameters = url.getQuery().split("=");
             if (parameters[0].equals("s")) {
                 str.add(parameters[1]);
-                return String.format("%s", str);
+                for (int i = 0; i < str.size(); i++){
+                    return String.format("%s", str.get(i));
+                }
             }
         }
-
         return "404 Not Found!";
     }
 }
 
-class NumberServer {
+class StringServer {
     public static void main(String[] args) throws IOException {
         if (args.length == 0) {
             System.out.println("Missing port number! Try any number between 1024 to 49151");
